70fcbb2e5cd162045dfbe4213185195c75c5f472
Lauren Ferreira <lauren.ferremch@gmail.com>
2017-09-03 16:36:14 -0300

    Agora a calculadora funciona

--- compute/compute.go
+++ compute/compute.go
@@ -4,14 +4,18 @@ import (
 	"strings"
 	"fmt"
 	"log"
+	"math"
+	"strconv"
+	"reflect"
+	"errors"
 )
 
 // Variável auxiliar para mostrar os logs na tela
 var print string
 
-// FIXME - A função não funciona corretamente - aceita funções inválidas
 // Analyze é a função que verifica se a expressão inserida é válida ou não
-func Analyze(expr string) bool {
+func Analyze(expr string) (bool, error) {
+
 	// Inicializa pilha
 	var stack Stack
 
@@ -23,37 +27,29 @@ func Analyze(expr string) bool {
 
 	// Entra no loop que varre a expressão
 	for _, r := range expr {
-		c := fmt.Sprintf("%c",r)
-		log.Println(c, r)
+		c := string(r)
+		print = fmt.Sprintf("Letra em análise: %s",c)
 		if c == "(" || c == "[" || c == "{" {
 			stack.Push(c)
-			log.Println("Entrou na entrada")
 		} else if c == ")" || c == "]" || c == "}" {
-			log.Println("Entrou na saída")
+			print = fmt.Sprintf("Entrou na saída: %s",c)
+			log.Println(print)
 			if stack.Empty() {
-				log.Println("A pilha está vazia antes do esperado")
-				return false
+				return false, errors.New("A pilha está vazia antes do esperado")
 			}
-			p := fmt.Sprintf("%c",stack.Pop())
-			// print = fmt.Sprintf("P = %d, C = %d",p,c)
-			// log.Println(print)
-			log.Println("P: ",p," C:", c)
+			p := stack.Pop().(string)
+			print = fmt.Sprintf("Comparando P = %s, C = %s",p,c)
+			log.Println(print)
 			if p == "(" && c != ")" {
-				log.Println("Há um caracter ')' sem inicializador")
-				return false
+				return false, errors.New("Há um caracter ')' sem inicializador")
 			} else if p == "[" && c != "]" {
-				log.Println("Há um caracter ']' sem inicializador")
-				return false
+				return false, errors.New("Há um caracter ']' sem inicializador")
 			} else if p == "{" && c != "}" {
-				log.Println("Há um caracter '}' sem inicializador")
-				return false
+				return false, errors.New("Há um caracter '}' sem inicializador")
 			}
-		} else {
-			log.Println("Entrou em nada")
-			return false
 		}
 	}
-	return stack.Empty()
+	return stack.Empty(),nil
 }
 
 // IsOperator verifica se o caracter é um operador
@@ -109,6 +105,7 @@ func Convert(expr string) string {
 		if char == " " {
 			continue
 		}
+
 		// Verifica os parênteses de entrada e saída
 		if char == "(" {
 			stack.Push(char)
@@ -132,8 +129,8 @@ func Convert(expr string) string {
 			postfix += " " + number
 			i = j - 1
 		} else {
-			// Se o caracter for um operador, desempilha dois elementos,
-			// faz a operação e empilha o resultado de volta
+			// Se o operando tiver prioridade menor, desempilha
+			// tudo até achar o parênteses inicial
 			for !stack.Empty() {
 				top, _ := stack.Top().(string)
 				if top == "(" || !HasMorePriority(top, char) {
@@ -153,19 +150,79 @@ func Convert(expr string) string {
 
 	print = fmt.Sprintf("A expressão %s foi convertida para pós-fixa: %s",expr, postfix)
 	log.Println(print)
+
 	// Retorna a expressão na notação pós-fixa sem espaços
 	return strings.TrimSpace(postfix)
 }
 
-// Calculate calcula o resultado da expressão pós-fixa
-// TODO - criar função calculadora
-func Calculate(posfix string) float32 {
-	//var stack Stack
+// IsNumeric verifica se uma parte de string é um número válido
+func IsNumeric(s string) bool {
+   _, err := strconv.ParseFloat(s, 64)
+   return err == nil
+}
 
-	print = fmt.Sprintf("Calculando expressão %s",posfix)
+// Calculate calcula o resultado da expressão pós-fixa
+func Calculate(postfix string) (float64, error) {
+
+	print = fmt.Sprintf("Calculando expressão %s",postfix)
 	log.Println(print)
-	
-	return 2.0
+
+	var stack Stack
+
+	for _, r := range postfix {
+		// r vem como rune, então precisamos converter para caracter string
+		c := string(r)
+		print = fmt.Sprintf("Verificando: %v",c)
+			log.Println(print)
+			// Se for número, coloca na pilha
+		if IsNumeric(c){
+			stack.Push(c)
+			// Ignora espaços em branco
+	} else if c != " " {
+			var a, b float64
+			// Pela pilha retornar uma interface, temos que confirmar se ela é
+			// float64 ou string e fazer os devidos tratamentos à variável	
+			if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.String{
+			b,_ = strconv.ParseFloat(stack.Pop().(string),64)
+			} else if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.Float64{
+			b,_ = stack.Pop().(float64)
+			}
+			if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.String{
+			a,_ = strconv.ParseFloat(stack.Pop().(string),64)
+			} else if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.Float64{
+			a,_ = stack.Pop().(float64)
+			}
+			print = fmt.Sprintf("A: %.2f | B: %.2f",a,b)
+			log.Println(print)
+			if c == "+" {
+				stack.Push(a+b)
+			} else if c == "-" {
+				stack.Push(a-b)
+			} else if c == "*" {
+				stack.Push(a*b)
+			} else if c == "/" {
+				stack.Push(a/b)
+			} else if c == "^" {
+				stack.Push(math.Pow(a, b))
+			}
+		}
+	}
+	// Retorna erro caso sobre mais de um valor na pilha
+	if stack.size > 1{
+		return 0, errors.New("Sobrou caracteres demais no final da conta")
+	} else {
+		// Mesmo caso relacionado à interface
+		if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.String{
+			resp,_ := strconv.ParseFloat(stack.Pop().(string),64)
+			return resp, nil
+			} else if v := reflect.TypeOf(stack.Top()); v.Kind() == reflect.Float64{
+			resp,_ := stack.Pop().(float64)
+			return resp, nil
+			} else {
+				// Retorna erro caso não seja tipo string nem float64
+			return 0, errors.New("Valor tem tipo irreconhecível")
+		}
+	}
 }
 
 // replace substitui os caracteres '[', ']', '{' e '}' por '(' e ')'
